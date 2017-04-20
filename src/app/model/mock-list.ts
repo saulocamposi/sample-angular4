c74c36f8f7ad73c751a8837e1b0e417ca422978d
Saulo Camposi <saulocamposi@gmail.com>
2017-04-20 20:08:29 +0100

    Add mock list schedule

---
+++ src/app/model/mock-list.ts
@@ -0,0 +1,7 @@
+import { Schedule } from './schedule';
+
+export const SCHEDULE: Schedule[] = [
+  {name:"First Schedule", description : "Description Schedule"},
+  {name:"Second Schedule", description : "Description Schedule"},
+  {name:"Third Schedule", description : "Description Schedule"}
+];
